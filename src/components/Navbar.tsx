d3c5c4e1a3b1278c35ee686b0b3290589dc7aaba
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-04-28 13:14:17 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,6 @@ const navLinks = [
   { name: 'Sport', path: '/sport' },
   { name: 'Activities', path: '/activities' },
   { name: 'General Application', path: '/admissions' },
-  { name: 'Boarding Application', path: '/boarding' },
   { name: 'Contact', path: '/contact' },
 ];
 
